699805576d1601ff075365904f88732d447509b4
Shawaiz Khan <shawaizkhan.biz@gmail.com>
2025-01-22 22:12:43 +0000

    add: Node and Inorder added

--- main.cpp
+++ main.cpp
@@ -2,76 +2,127 @@
 using namespace std;
 
 class Node {
+public:
     int data;
     Node* right;
     Node* left;
     int height;
 
-    public:
-        Node(int val) {
-            data = val;
-            right = NULL;
-            left = NULL;
-            height = 1;
-        }
-}
-
-Node* createNode(int val) {
-    Node* newNode = new Node(val);
-    return newNode;
-}
+    Node(int val) {
+        data = val;
+        right = NULL;
+        left = NULL;
+        height = 1;
+    }
+};
 
 int getHeight(Node* node) {
     if (node == NULL) {
         return 0;
-    } else {
-        return node->height;
     }
+    return node->height;
 }
 
 int getBalanceFactor(Node* node) {
     if (node == NULL) {
         return 0;
-    } else {
-        return getHeight(node->left) - getHeight(node->right);
     }
+    return getHeight(node->left) - getHeight(node->right);
 }
 
 void updateHeight(Node* node) {
     if (node != NULL) {
         int leftHeight = getHeight(node->left);
         int rightHeight = getHeight(node->right);
-
-        if (leftHeight > rightHeight) {
-            node->height = leftHeight + 1;
-        } else {
-            node->height = rightHeight + 1;
-        }
+        node->height = (leftHeight > rightHeight ? leftHeight : rightHeight) + 1;
     }
 }
 
-void insert(Node* root, int val) {
+Node* rotateRight(Node* y) {
+    Node* x = y->left;
+    Node* T = x->right;
+
+    x->right = y;
+    y->left = T;
+
+    updateHeight(y);
+    updateHeight(x);
+
+    return x;
+}
+
+Node* rotateLeft(Node* x) {
+    Node* y = x->right;
+    Node* T = y->left;
+
+    y->left = x;
+    x->right = T;
+
+    updateHeight(x);
+    updateHeight(y);
+
+    return y;
+}
+
+Node* insert(Node* root, int val) {
     if (root == NULL) {
-        root = createNode(val);
-        cout << val << " inserted at root" << endl;
-    } else if (val < root->data) {
+        return new Node(val);
+    }
+
+    if (val < root->data) {
         root->left = insert(root->left, val);
-    } else if (val > root->data) {
+    } 
+    else if (val > root->data) {
         root->right = insert(root->right, val);
-    } else {
-        cout << "No Duplicates allowed";
+    } 
+    else {
+        cout << "No Duplicates allowed\n";
         return root;
     }
 
     updateHeight(root);
-
     int balance = getBalanceFactor(root);
+
+    if (balance > 1 && val < root->left->data) {
+        return rotateRight(root);
+    }
+    if (balance < -1 && val > root->right->data) {
+        return rotateLeft(root);
+    }
+    if (balance > 1 && val > root->left->data) {
+        root->left = rotateLeft(root->left);
+        return rotateRight(root);
+    }
+    if (balance < -1 && val < root->right->data) {
+        root->right = rotateRight(root->right);
+        return rotateLeft(root);
+    }
+
+    return root;
+}
+
+void inorder(Node* root) {
+    if (root == NULL) {
+        return;
+    }
+    inorder(root->left);
+    cout << root->data << " ";
+    inorder(root->right);
 }
 
 int main() {
-    Node* root;
-    root = NULL;
+    Node* root = NULL;
 
-    insert(root, 10);
+    root = insert(root, 10);
+    root = insert(root, 20);
+    root = insert(root, 30);
+    root = insert(root, 40);
+    root = insert(root, 50);
+    root = insert(root, 25);
+
+    cout << "In-order Traversal: ";
+    inorder(root);
+    cout << endl;
+
     return 0;
 }
